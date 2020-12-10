4be0a98f2ce15b1418d4f139ee135f7cab1baa06
Alex Racinski <aracinski2@gmail.com>
2020-12-10 15:14:16 -0600

    add image_url to html, remove button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+
+    <div v-for="product in products">
+      <h3>{{ product.name }}</h3>
+      <img v-bind:src="product.image_url">
+      <p>${{ product.price }}</p>
+    </div>
+    <!-- <button v-on:click="productsIndex">Index</button> -->
+
   </div>
 </template>
 <style>
 </style>
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    console.log("in created");
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      console.log("products index......");
+      axios.get("/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
