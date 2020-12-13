549384f59d2f203f3f9d21e014946b07bce5e58d
Alex Racinski <aracinski2@gmail.com>
2020-12-13 10:52:41 -0600

    add XMLH, add create product to method/template with button and text boxes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,18 @@
   <div class="home">
     <h1>{{ message }}</h1>
 
+    <button v-on:click="createProduct()">Create Product</button>
+    <p>Name:<input type="text" v-model="name"></p>
+    <p>Price:<input type="text" v-model="price"></p>
+    <p>Description:<input type="text" v-model="description"></p>
+    <p>Image_url:<input type="text" v-model="image_url"></p>
+
     <div v-for="product in products">
       <h3>{{ product.name }}</h3>
-      <img v-bind:src="product.image_url">
+      <p><img v-bind:src="product.image_url" v-bind:alt="product.name"></p>
       <p>${{ product.price }}</p>
     </div>
     <!-- <button v-on:click="productsIndex">Index</button> -->
-
   </div>
 </template>
 <style>
@@ -20,6 +25,10 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       products: [],
+      name: "",
+      price: "",
+      description: "",
+      image_url: "",
     };
   },
   created: function () {
@@ -32,6 +41,19 @@ export default {
       axios.get("/api/products").then((response) => {
         console.log(response.data);
         this.products = response.data;
+      });
+    },
+    createProduct: function () {
+      console.log("create function");
+      var params = {
+        name: this.name,
+        price: this.price,
+        description: this.description,
+        image_url: this.image_url,
+      };
+      axios.post("/api/products", params).then((response) => {
+        console.log(response.data);
+        this.products.push(response.data);
       });
     },
   },
